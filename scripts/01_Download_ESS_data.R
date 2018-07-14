e7e1470278926a597c7331adad99779879fe03c2
JosepER <jespasareig@gmail.com>
2018-07-14 01:48:20 +0100

    gitignore update

--- scripts/01_Download_ESS_data.R
+++ scripts/01_Download_ESS_data.R
@@ -1,2 +1,17 @@
 # 01_Download_ESS_data
 
+library(here)
+
+if(!file.exists(here("data", "from_ESS", "ESS8e02.spss.zip"))){
+
+download.file(url = "http://www.europeansocialsurvey.org/file/download?f=ESS8e02.spss.zip&c=&y=2016",
+              destfile = here("data", "from_ESS", "ESS8e02.spss.zip"))
+
+}
+
+
+zip.file.extract(file = "ESS8e02.sav", 
+                 zipname = here("data", "from_ESS", "ESS8e02.spss.zip"))
+
+unzip(zipfile = here("data", "from_ESS", "ESS8e02.spss.zip"),
+                     exdir= here("data", "from_ESS", "ESS8e02.sav"))
